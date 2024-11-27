bb8b7855f867fff9a36d518bd52705cb38035c1d
moselle123 <m.pattison.christie@gmail.com>
2024-11-27 23:29:52 +0000

    display logout button based on router name

--- client/App.vue
+++ client/App.vue
@@ -5,7 +5,7 @@
 				<el-menu-item>
 					<el-text size="large" tag="b">Shangri-La Petition Platform</el-text>
 				</el-menu-item>
-				<el-menu-item @click="logout">Logout</el-menu-item>
+				<el-menu-item v-if="pageName !== 'Login' && pageName !== 'Register'" @click="logout">Logout</el-menu-item>
 			</el-menu>
 		</el-header>
 		<el-main>
@@ -15,10 +15,10 @@
 </template>
 <script>
 export default {
-	data() {
-		return {
-
-		};
+	computed: {
+		pageName() {
+			return this.$route.name;
+		},
 	},
 	methods: {
 		logout() {
